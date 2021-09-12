9e92591c19a10eb7737289d2999c2d86769a94e7
st1020 <stone_1020@qq.com>
2021-09-12 14:59:15 +0800

    feat(plugin_send): 添加 send 插件

---
+++ plugins/alicebot_plugin_send/__init__.py
@@ -0,0 +1,26 @@
+import re
+
+from plugins.alicebot_plugin_public import BasePlugin
+
+from .config import Config
+
+
+class Send(BasePlugin[Config]):
+    plugin_config_class: Config = Config
+
+    def __post_init__(self):
+        self.re_pattern = re.compile(
+            f'({"|".join(self.plugin_config.command_prefix | getattr(self.config, "command_prefix", set()))})' +
+            f'({"|".join(self.plugin_config.command)})' + r'\s*(?P<message>.*)',
+            flags=re.I
+        )
+
+    async def handle(self) -> None:
+        try:
+            await self.send(self.msg_match.group('message'), 'private', self.plugin_config.send_user_id)
+        except Exception as e:
+            if self.plugin_config.send_filed_msg is not None:
+                await self.event.reply(self.format_str(self.plugin_config.send_filed_msg, repr(e)))
+        else:
+            if self.plugin_config.send_success_msg is not None:
+                await self.event.reply(self.format_str(self.plugin_config.send_success_msg))
